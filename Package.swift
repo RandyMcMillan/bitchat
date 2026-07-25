c484359fc1280c0cc853d78e6f74010c39b0e228
randymcmillan <randymcmillan@protonmail.com>
2026-07-25 13:37:24 -0400

    Package.swift:library

--- Package.swift
+++ Package.swift
@@ -9,28 +9,39 @@ let package = Package(
         .macOS(.v13)
     ],
     products: [
+        .library(
+            name: "Bitchat",
+            targets: ["Bitchat"]
+        ),
         .executable(
             name: "bitchat",
-            targets: ["bitchat"]
+            targets: ["BitchatApp"]
         ),
     ],
     dependencies:[
         .package(url: "https://github.com/21-DOT-DEV/swift-secp256k1", exact: "0.21.1"),
     ],
     targets: [
-        .executableTarget(
-            name: "bitchat",
+        .target(
+            name: "Bitchat",
             dependencies: [
                 .product(name: "P256K", package: "swift-secp256k1")
             ],
             path: "bitchat",
             exclude: [
+                "BitchatApp.swift",
                 "Info.plist",
                 "Assets.xcassets",
                 "bitchat.entitlements",
                 "bitchat-macOS.entitlements",
                 "LaunchScreen.storyboard"
             ]
+        ),
+        .executableTarget(
+            name: "BitchatApp",
+            dependencies: ["Bitchat"],
+            path: "bitchat",
+            sources: ["BitchatApp.swift"]
         ),
     ]
 )
--- bitchat/BitchatApp.swift
+++ bitchat/BitchatApp.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import UserNotifications
+import Bitchat
 
 @main
 struct BitchatApp: App {
